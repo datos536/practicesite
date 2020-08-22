215d6d4f21d3209715d074e914973371544862dd
JC <68460260+datos536@users.noreply.github.com>
2020-08-22 17:17:13 +0900

    Target: JS functionality to change picture, using button

---
+++ script.js
@@ -0,0 +1,8 @@
+/*alert("Welcome to our page");*/
+document.getElementById("buttonpic").onclick = function () {
+    document.getElementById("pic1").src="./images/001.jpg";
+}
+
+document.getElementById("resetpic").onclick = function () {
+    document.getElementById("pic1").src="./images/000.jpg";
+}
